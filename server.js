efc8ab5ff71241ed75f0360266a8cef56437a236
jonathan seneris <jonathanpseneris@gmail.com>
2015-06-30 19:46:56 -0700

    fixed all lobsters

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.listen(config.port);
 exports = module.exports = app;
 
 // Logging initialization
-console.log('LOBSTERDEATH application started on port ' + config.port);
+console.log('StageUp application started on port ' + config.port);
